e6327a410c7c8badad22ab7e3ac10ceeffa9e475
Zweihänder <zweidev@zweihander.me>
2022-02-26 22:14:31 -0500

    feat(app): basic cra-like scaffold

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="app">
+			<header className="app-header">
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="app-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
