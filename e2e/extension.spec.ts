68d82258e030e826de788dac7495308785bcaa33
Zweihänder <zweidev@zweihander.me>
2023-07-03 19:55:37 -0400

    test(e2e): only test ws status

--- e2e/extension.spec.ts
+++ e2e/extension.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from './fixture';
 
-const MASTER_MESSAGE = 'Closed - Master';
-const CLIENT_MESSAGE = 'Uninstantiated - Client';
+const MASTER_MESSAGE = 'Master';
+const CLIENT_MESSAGE = 'Client';
 const STATUS_LOCATOR = 'connection-status';
 
 test('newtab page', async ({ page, extensionId }) => {
 	await page.goto(`chrome-extension://${extensionId}/newtab.html`);
-	await expect(page.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
+	await expect(page.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
 });
 
 test('multiple tabs', async ({ extensionId, context }) => {
@@ -14,8 +16,12 @@ test('multiple tabs', async ({ extensionId, context }) => {
 	const tab2 = await context.newPage();
 	await tab1.goto(`chrome-extension://${extensionId}/newtab.html`);
 	await tab2.goto(`chrome-extension://${extensionId}/newtab.html`);
-	await expect(tab1.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
-	await expect(tab2.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
+	await expect(tab1.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await expect(tab2.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
 });
 
 test('tab query flow', async ({ extensionId, context }) => {
@@ -25,24 +31,50 @@ test('tab query flow', async ({ extensionId, context }) => {
 	await tab1.goto(`chrome-extension://${extensionId}/newtab.html`);
 	await tab2.goto(`chrome-extension://${extensionId}/newtab.html`);
 	await tab3.goto(`chrome-extension://${extensionId}/newtab.html`);
-	await expect(tab1.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
-	await expect(tab2.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
-	await expect(tab3.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
+	await expect(tab1.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await expect(tab2.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
+	await expect(tab3.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
 	await tab1.close();
-	await expect(tab2.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
-	await expect(tab3.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
+	await expect(tab2.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await expect(tab3.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
 	const tab4 = await context.newPage();
 	await tab4.goto(`chrome-extension://${extensionId}/newtab.html`);
-	await expect(tab4.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
+	await expect(tab4.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
 	await tab2.reload();
-	await expect(tab3.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
-	await expect(tab2.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
-	await expect(tab4.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
+	await expect(tab3.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await expect(tab2.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
+	await expect(tab4.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
 	await tab2.close();
-	await expect(tab3.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
-	await expect(tab4.getByTestId(STATUS_LOCATOR)).toHaveText(CLIENT_MESSAGE);
+	await expect(tab3.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await expect(tab4.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
 	await tab3.close();
-	await expect(tab4.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
+	await expect(tab4.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
 	await tab4.reload();
-	await expect(tab4.getByTestId(STATUS_LOCATOR)).toHaveText(MASTER_MESSAGE);
+	await expect(tab4.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
 });
