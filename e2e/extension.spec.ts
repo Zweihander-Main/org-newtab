1e0157088dca8e287aafd7adcbf60a782f6c2de3
Zweihänder <zweidev@zweihander.me>
2023-07-03 21:41:25 -0400

    test(e2e): create failing close/open flow

--- e2e/extension.spec.ts
+++ e2e/extension.spec.ts
@@ -78,3 +78,38 @@ test('tab query flow', async ({ extensionId, context }) => {
 		MASTER_MESSAGE
 	);
 });
+
+test('tab query flow stopping and starting', async ({
+	extensionId,
+	context,
+}) => {
+	const tab1 = await context.newPage();
+	const tab2 = await context.newPage();
+	const tab3 = await context.newPage();
+	await tab1.goto(`chrome-extension://${extensionId}/newtab.html`);
+	await tab2.goto(`chrome-extension://${extensionId}/newtab.html`);
+	await tab3.goto(`chrome-extension://${extensionId}/newtab.html`);
+	await expect(tab1.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await expect(tab2.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
+	await expect(tab3.getByTestId(STATUS_LOCATOR)).toContainText(
+		CLIENT_MESSAGE
+	);
+	await tab1.close();
+	await tab2.close();
+	await tab3.close();
+	const tab4 = await context.newPage();
+	await tab4.goto(`chrome-extension://${extensionId}/newtab.html`);
+	await expect(tab4.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await tab4.close();
+	await tab4.goto(`chrome-extension://${extensionId}/newtab.html`);
+	await expect(tab4.getByTestId(STATUS_LOCATOR)).toContainText(
+		MASTER_MESSAGE
+	);
+	await tab4.close();
+});
