8f62256986e9540e282be6dcbda0ab1e72cbc792
Zweihänder <zweidev@zweihander.me>
2023-07-18 16:41:21 -0400

    test(usesinglews): update test name

--- src/hooks/__tests__/useSingleWS.test.tsx
+++ src/hooks/__tests__/useSingleWS.test.tsx
@@ -4,7 +4,7 @@ import useSingleWebSocket from '../useSingleWS';
 describe('AppContext', () => {
 	const { result } = renderHook(() => useSingleWebSocket());
 
-	it('should return readyState of -1', () => {
+	it('should start as client ws', () => {
 		expect(result.current.amMasterWS).toBe(false);
 	});
 });
