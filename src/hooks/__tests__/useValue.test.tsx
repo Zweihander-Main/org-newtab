08459a05226d412ececbcb7246a47fbac67e1420
Zweihänder <zweidev@zweihander.me>
2023-07-20 21:04:49 -0400

    feat(usevalue): merge useStorage
    Merged useStorage into useValue including combining the tests. The
    tests were a more useful exercise than getting rid of the outside
    dep except that it showed exactly what needs to change to stop re-render
    bugs.

--- src/hooks/__tests__/useValue.test.tsx
+++ src/hooks/__tests__/useValue.test.tsx
@@ -1,13 +1,61 @@
-import { renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import useValue from '../useValue';
 
 describe('useValue', () => {
-	const { result } = renderHook(() => useValue('readyState'));
+	afterEach(async () => {
+		await chrome.storage.local.clear();
+		jest.clearAllMocks();
+	});
 
-	it('should ask storage for result', async () => {
-		expect(result.current.value).toBe(-1);
+	it('should use initial value', async () => {
+		const { result } = renderHook(() => useValue('readyState'));
 		await waitFor(() => {
-			expect(chrome.storage.local.get).toHaveBeenCalled();
+			expect(result.current.value).toBe(-1);
+		});
+	});
+
+	it('should ask storage for initial value', async () => {
+		renderHook(() => useValue('readyState'));
+		await waitFor(() => {
+			expect(chrome.storage.local.get).toHaveBeenCalledWith(
+				{
+					readyState: -1,
+				},
+				expect.any(Function)
+			);
+		});
+	});
+
+	it('should set value', async () => {
+		const { result } = renderHook(() => useValue('readyState'));
+		act(() => {
+			result.current.setValue(1);
+		});
+		await waitFor(() => {
+			expect(result.current.value).toBe(1);
+		});
+	});
+
+	it('should set value in storage', async () => {
+		const { result } = renderHook(() => useValue('readyState'));
+		act(() => {
+			result.current.setValue(1);
+		});
+		await waitFor(() => {
+			expect(chrome.storage.local.set).toHaveBeenCalledWith(
+				{
+					readyState: 1,
+				},
+				expect.any(Function)
+			);
+		});
+	});
+
+	it('should subscribe to storage listener', async () => {
+		expect(chrome.storage.onChanged.addListener).not.toHaveBeenCalled();
+		renderHook(() => useValue('readyState'));
+		await waitFor(() => {
+			expect(chrome.storage.onChanged.addListener).toHaveBeenCalled();
 		});
 	});
 });
--- src/hooks/useValue.tsx
+++ src/hooks/useValue.tsx
@@ -1,67 +1,117 @@
+import { ReadyState } from 'react-use-websocket';
+import type { EmacsItemMsg } from '../lib/types';
 import { useCallback, useEffect, useState } from 'react';
-import useAppState, { type AppState } from './useAppState';
+
+export interface AppState {
+	matchQuery: string | undefined;
+	tagsData: { [key: string]: string | null };
+	orgItem: EmacsItemMsg['data'] | null;
+	readyState: ReadyState;
+	isWaitingForResponse: boolean;
+}
+
+const INITIAL_VALUE: AppState = {
+	matchQuery: 'TODO="TODO"',
+	tagsData: {},
+	orgItem: null,
+	readyState: ReadyState.UNINSTANTIATED,
+	isWaitingForResponse: false,
+};
+
+export const storage = {
+	get: <T extends keyof AppState>(
+		key: T,
+		defaultValue?: (typeof INITIAL_VALUE)[T]
+	) => {
+		const keyObj =
+			defaultValue === undefined ? key : { [key]: defaultValue };
+		return new Promise<AppState[T]>((resolve, reject) => {
+			chrome.storage.local.get(keyObj, (items) => {
+				const error = chrome.runtime.lastError;
+				if (error) return reject(error);
+				resolve(items[key] as AppState[T]);
+			});
+		});
+	},
+	set: <T extends keyof AppState>(key: T, value: AppState[T]) => {
+		return new Promise<void>((resolve, reject) => {
+			chrome.storage.local.set({ [key]: value }, () => {
+				const error = chrome.runtime.lastError;
+				error ? reject(error) : resolve();
+			});
+		});
+	},
+};
+
+const isEqual = <T extends keyof AppState>(a: AppState[T], b: AppState[T]) => {
+	if (typeof a === 'object') {
+		return JSON.stringify(a) === JSON.stringify(b);
+	}
+	return a === b;
+};
 
 /**
  * @param key which key to query
- * @returns {value, setValue, isPersistent, isInitialStateResolved}
+ * @returns {value, setValue, isInitialStateResolved}
  */
-const useValue = <T extends keyof AppState>(
-	key: T
-): {
-	value: AppState[T];
-	setValue: (newValue: AppState[T]) => void;
-	isPersistent: boolean;
-	isInitialStateResolved: boolean;
-} => {
-	const [appState, setAppState, isPersistent, error, isInitialStateResolved] =
-		useAppState();
-
-	// NEXT: create a state variable about what sort of value it is if possible
-	// NEXT: Write test for useValue, mock the storage and confirm it's being hit how many times
-
-	const [value, setValue] = useState(appState[key]);
-
-	if (
-		(typeof value === 'object' &&
-			JSON.stringify(value) !== JSON.stringify(appState[key])) ||
-		(typeof value !== 'object' && value !== appState[key])
-	) {
-		setValue(appState[key]);
-	}
-
-	const [isPersistentValue, setIsPersistentValue] = useState(isPersistent);
-
-	if (isPersistentValue !== isPersistent) {
-		setIsPersistentValue(isPersistent);
-	}
-
-	const [isInitialStateResolvedValue, setIsInitialStateResolvedValue] =
-		useState(isInitialStateResolved);
-
-	if (isInitialStateResolvedValue !== isInitialStateResolved) {
-		setIsInitialStateResolvedValue(isInitialStateResolved);
-	}
-
-	/**
-	 * Note that the imported library will always call set.storage if you call
-	 * the set function. There's no returning null path here.
-	 */
-	const setStorageValue = useCallback(
-		(newValue: AppState[T]) => {
-			if (
-				typeof newValue === 'object' &&
-				JSON.stringify(newValue) === JSON.stringify(value)
-			) {
-				return;
-			} else if (typeof newValue !== 'object' && value === newValue) {
-				return;
-			}
-			setAppState((prevState) => {
-				return { ...prevState, [key]: newValue };
-			});
-		},
-		[key, setAppState, value]
+const useValue = <T extends keyof AppState>(key: T) => {
+	const [cachedValue, setCachedValue] = useState<AppState[T]>(
+		INITIAL_VALUE[key]
 	);
+	const [error, setError] = useState<undefined | string>(undefined);
+	const [isInitialStateResolved, setIsInitialStateResolved] = useState(false);
+
+	useEffect(() => {
+		storage
+			.get(key, INITIAL_VALUE[key])
+			.then((res) => {
+				setCachedValue(res);
+				setError(undefined);
+				setIsInitialStateResolved(true);
+			})
+			.catch((error: chrome.runtime.LastError) => {
+				setError(error.message);
+				setIsInitialStateResolved(true);
+			});
+	}, [key]);
+
+	const setValue = useCallback(
+		(newValue: AppState[T]) => {
+			if (isEqual(newValue, cachedValue)) return;
+			setCachedValue(newValue);
+			storage
+				.set(key, newValue)
+				.catch((error: chrome.runtime.LastError) => {
+					setError(error.message);
+				});
+		},
+		[cachedValue, key]
+	);
+
+	useEffect(() => {
+		const onChange: Parameters<
+			typeof chrome.storage.onChanged.addListener
+		>[0] = (changes, areaName) => {
+			if (areaName === 'local' && key in changes) {
+				const { newValue, prevValue } = changes[key].newValue as {
+					newValue: AppState[T];
+					prevValue: AppState[T];
+				};
+				if (!isEqual(newValue, prevValue)) {
+					setCachedValue(changes[key].newValue as AppState[T]);
+					setError(undefined);
+				}
+			}
+		};
+		if (!chrome.storage.onChanged.hasListener(onChange)) {
+			// eslint-disable-next-line no-console
+			console.log('Adding listener for', key);
+			chrome.storage.onChanged.addListener(onChange);
+		}
+		return () => {
+			chrome.storage.onChanged.removeListener(onChange);
+		};
+	}, [key]);
 
 	useEffect(() => {
 		if (error && error !== '') {
@@ -70,11 +120,12 @@ const useValue = <T extends keyof AppState>(
 	}, [error, key]);
 
 	return {
-		value,
-		setValue: setStorageValue,
-		isPersistent: isPersistentValue,
-		isInitialStateResolved: isInitialStateResolvedValue,
+		value: cachedValue,
+		setValue,
+		isInitialStateResolved,
 	};
 };
+
+// TODO: remove usechromestorage
 
 export default useValue;
