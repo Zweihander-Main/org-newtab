b01abacf511454fb7963ea7fd509f59062d0779f
Zweihänder <zweidev@zweihander.me>
2023-07-29 16:03:59 -0400

    test(e2e/ws): add in check for item

--- e2e/ws.spec.ts
+++ e2e/ws.spec.ts
@@ -1,5 +1,6 @@
 import {
 	CLIENT_MESSAGE,
+	GET_ITEM_COMMAND,
 	HOW_LONG_TO_WAIT_FOR_WEBSOCKET,
 	MASTER_MESSAGE,
 	STATUS_LOCATOR,
@@ -19,6 +20,11 @@ function startTestWebSocketServer() {
 		ws.on('message', (message) => {
 			// eslint-disable-next-line no-console
 			console.log('Received message from client:', message);
+		});
+
+		ws.on('close', () => {
+			// eslint-disable-next-line no-console
+			console.log('Client disconnected');
 		});
 	});
 	return wss;
@@ -42,67 +48,111 @@ async function isPortInUse(port: number) {
 	});
 }
 
-let websocketServer: WebSocket.Server | undefined;
+test.describe('WebSocket', () => {
+	test.describe.configure({ mode: 'serial' });
 
-test.beforeEach(async () => {
-	if (!(await isPortInUse(WEBSOCKET_PORT))) {
-		websocketServer = startTestWebSocketServer();
-	}
-});
+	let websocketServer: WebSocket.Server | undefined;
+	test.beforeEach(async () => {
+		if (!(await isPortInUse(WEBSOCKET_PORT))) {
+			websocketServer = startTestWebSocketServer();
+		}
+	});
 
-test.afterEach(() => {
-	if (websocketServer) {
-		websocketServer.close();
-	}
-});
+	test.afterEach(() => {
+		if (websocketServer) {
+			websocketServer.close();
+		}
+	});
 
-test('Should open a websocket connection to emacs from the master tab', async ({
-	extensionId,
-	context,
-}) => {
-	const tab1 = await context.newPage();
-	async function waitForWebsocket(): Promise<boolean> {
-		return new Promise(function (resolve) {
-			tab1.on('websocket', (ws) => {
-				if (ws.url() === WEBSOCKET_URL) {
-					resolve(true);
-				}
+	test('Should open a connection to emacs from the master tab', async ({
+		extensionId,
+		context,
+	}) => {
+		const tab1 = await context.newPage();
+		async function websocketOpened(): Promise<boolean> {
+			return new Promise(function (resolve) {
+				tab1.on('websocket', (ws) => {
+					if (ws.url() === WEBSOCKET_URL) {
+						resolve(true);
+					}
+				});
+				setTimeout(
+					() => resolve(false),
+					HOW_LONG_TO_WAIT_FOR_WEBSOCKET
+				);
 			});
-			setTimeout(() => resolve(false), HOW_LONG_TO_WAIT_FOR_WEBSOCKET);
-		});
-	}
+		}
 
-	await tab1.goto(`chrome-extension://${extensionId}/newtab.html`);
-	await expect(tab1.getByTestId(STATUS_LOCATOR)).toContainText(
-		MASTER_MESSAGE
-	);
-	expect(await waitForWebsocket()).toBeTruthy();
-});
+		await tab1.goto(`chrome-extension://${extensionId}/newtab.html`);
+		await expect(tab1.getByTestId(STATUS_LOCATOR)).toContainText(
+			MASTER_MESSAGE
+		);
+		expect(await websocketOpened()).toBeTruthy();
+	});
 
-test('Should not open a websocket connection to emacs from a client tab', async ({
-	extensionId,
-	context,
-}) => {
-	const tabMaster = await context.newPage();
-	const tabClient = await context.newPage();
-	async function waitForClientWebsocket(): Promise<boolean> {
-		return new Promise(function (resolve) {
-			tabClient.on('websocket', (ws) => {
-				if (ws.url() === WEBSOCKET_URL) {
-					resolve(true);
-				}
+	test('Should not open a connection to emacs from a client tab', async ({
+		extensionId,
+		context,
+	}) => {
+		const tabMaster = await context.newPage();
+		const tabClient = await context.newPage();
+		async function clientWebsocketOpened(): Promise<boolean> {
+			return new Promise(function (resolve) {
+				tabClient.on('websocket', (ws) => {
+					if (ws.url() === WEBSOCKET_URL) {
+						resolve(true);
+					}
+				});
+				setTimeout(
+					() => resolve(false),
+					HOW_LONG_TO_WAIT_FOR_WEBSOCKET
+				);
 			});
-			setTimeout(() => resolve(false), HOW_LONG_TO_WAIT_FOR_WEBSOCKET);
-		});
-	}
+		}
 
-	await tabMaster.goto(`chrome-extension://${extensionId}/newtab.html`);
-	await tabClient.goto(`chrome-extension://${extensionId}/newtab.html`);
-	await expect(tabMaster.getByTestId(STATUS_LOCATOR)).toContainText(
-		MASTER_MESSAGE
-	);
-	await expect(tabClient.getByTestId(STATUS_LOCATOR)).toContainText(
-		CLIENT_MESSAGE
-	);
-	expect(await waitForClientWebsocket()).toBeFalsy();
+		await tabMaster.goto(`chrome-extension://${extensionId}/newtab.html`);
+		await tabClient.goto(`chrome-extension://${extensionId}/newtab.html`);
+		await expect(tabMaster.getByTestId(STATUS_LOCATOR)).toContainText(
+			MASTER_MESSAGE
+		);
+		await expect(tabClient.getByTestId(STATUS_LOCATOR)).toContainText(
+			CLIENT_MESSAGE
+		);
+		expect(await clientWebsocketOpened()).toBeFalsy();
+	});
+
+	test('Should ask for item after opening', async ({
+		extensionId,
+		context,
+	}) => {
+		const tab1 = await context.newPage();
+		async function getItemMessageSent(): Promise<boolean> {
+			return new Promise(function (resolve) {
+				tab1.on('websocket', (ws) => {
+					if (ws.url() === WEBSOCKET_URL) {
+						ws.on('framesent', (data) => {
+							const json = JSON.parse(data.payload as string) as {
+								command?: string;
+							};
+							if (json?.command === GET_ITEM_COMMAND) {
+								resolve(true);
+							}
+						});
+						ws.on('close', () => resolve(false));
+						ws.on('socketerror', () => resolve(false));
+					}
+				});
+				setTimeout(
+					() => resolve(false),
+					HOW_LONG_TO_WAIT_FOR_WEBSOCKET * 20
+				);
+			});
+		}
+
+		await tab1.goto(`chrome-extension://${extensionId}/newtab.html`);
+		await expect(tab1.getByTestId(STATUS_LOCATOR)).toContainText(
+			MASTER_MESSAGE
+		);
+		expect(await getItemMessageSent()).toBeTruthy();
+	});
 });
