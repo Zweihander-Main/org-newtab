d27e5997cda3ddda5d3594dbf26ed135a1efc763
Zweihänder <zweidev@zweihander.me>
2023-08-10 17:12:11 -0400

    fix(options): clean up bar spacing

--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -5,7 +5,7 @@ import { PersistGate } from '@plasmohq/redux-persist/integration/react';
 import '@fontsource/public-sans/700.css';
 import './index.css';
 import ConnectionStatusIndicator from 'components/ConnectionStatusIndicator';
-import OptionsMenu from 'components/OptionsMenu';
+import OptionsMenu from 'components/Options';
 import OrgItem from 'components/OrgItem';
 import LoadingBar from 'components/LoadingBar';
 import store, { persistor } from '../app/store';
