defc4f4b2bf980db8613712b5e72bafbf80a3e8a
Zweihänder <zweidev@zweihander.me>
2023-08-16 22:49:05 -0400

    refactor(layout): implement more verbose drag pattern

--- src/components/LayoutPanel/index.tsx
+++ src/components/LayoutPanel/index.tsx
@@ -1,93 +1,141 @@
-import { useState } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 import * as styles from './style.module.css';
 import {
-	useDroppable,
 	useDraggable,
 	DndContext,
+	UniqueIdentifier,
+	DraggableSyntheticListeners,
+	DragEndEvent,
+	useDroppable,
+	useDndContext,
 	DragOverlay,
 } from '@dnd-kit/core';
+import { createPortal } from 'react-dom';
 
-const Item: React.FC = () => {
-	return (
-		<div className={styles.component}>
-			Connection Status Indicator
-			<span className={styles.remove}>X</span>
-		</div>
-	);
-};
-
-type DraggableItemProps = {
+interface DroppableProps {
 	children: React.ReactNode;
-	id: string;
-	element?: React.ElementType;
-};
+	dragging: boolean;
+	id: UniqueIdentifier;
+}
 
-const DraggableItem: React.FC<DraggableItemProps> = ({
-	children,
-	id,
-	element,
-}) => {
-	const Element = element || 'div';
-	const { attributes, listeners, setNodeRef } = useDraggable({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Droppable: React.FC<DroppableProps> = ({ children, id, dragging }) => {
+	const { setNodeRef } = useDroppable({
 		id,
 	});
 
 	return (
-		<Element ref={setNodeRef} {...listeners} {...attributes}>
+		<div ref={setNodeRef} aria-label="Droppable region">
 			{children}
-		</Element>
+		</div>
+	);
+};
+
+interface DraggableProps {
+	children?: React.ReactNode;
+	dragOverlay?: boolean;
+	dragging?: boolean;
+	listeners?: DraggableSyntheticListeners;
+	style?: React.CSSProperties;
+}
+
+const Draggable = forwardRef<HTMLButtonElement, DraggableProps>(
+	function Draggable({ listeners, style, ...props }, ref) {
+		return (
+			<div
+				style={{
+					...style,
+				}}
+			>
+				<button
+					{...props}
+					aria-label="Draggable"
+					{...listeners}
+					ref={ref}
+				>
+					{props.children}
+				</button>
+			</div>
+		);
+	}
+);
+
+const DraggableItem: React.FC = () => {
+	const { isDragging, setNodeRef, listeners } = useDraggable({
+		id: 'draggable-item',
+	});
+
+	return (
+		<Draggable
+			dragging={isDragging}
+			ref={setNodeRef}
+			listeners={listeners}
+			style={{
+				opacity: isDragging ? 0 : undefined,
+			}}
+		>
+			Drag me
+		</Draggable>
+	);
+};
+
+const DraggableOverlay: React.FC = () => {
+	const { active } = useDndContext();
+
+	return createPortal(
+		<DragOverlay>
+			{active ? <Draggable dragging dragOverlay /> : null}
+		</DragOverlay>,
+		document.body
 	);
 };
 
 const LayoutPanel: React.FC = () => {
-	const { setNodeRef: setTopAreaDroppableRef } = useDroppable({
-		id: 'top-area',
-	});
-	const { setNodeRef: setMidAreaDroppableRef } = useDroppable({
-		id: 'mid-area',
-	});
-	const { setNodeRef: setBottomAreaDroppableRef } = useDroppable({
-		id: 'bottom-area',
-	});
-
 	const [isDragging, setIsDragging] = useState(false);
+	const [parent, setParent] = useState<UniqueIdentifier | null>(null);
 
-	function handleDragStart() {
+	const item = <DraggableItem />;
+
+	const handleDragStart = () => {
 		setIsDragging(true);
-	}
+	};
 
-	function handleDragEnd() {
+	const handleDragEnd = ({ over }: DragEndEvent) => {
 		setIsDragging(false);
-	}
+		setParent(over ? over.id : null);
+	};
+
+	const handleDragCancel = () => {
+		setIsDragging(false);
+	};
+
+	useEffect(() => {
+		setParent('bottom');
+	}, []);
 
 	return (
-		<DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+		<DndContext
+			onDragStart={handleDragStart}
+			onDragEnd={handleDragEnd}
+			onDragCancel={handleDragCancel}
+		>
 			<div className={styles.map}>
-				<div ref={setTopAreaDroppableRef} className={styles.area}>
+				<Droppable key={'top'} id={'top'} dragging={isDragging}>
+					{parent === 'top' ? item : null}
 					Top
-				</div>
-				<div ref={setMidAreaDroppableRef} className={styles.area}>
+				</Droppable>
+				<Droppable key={'mid'} id={'mid'} dragging={isDragging}>
+					{parent === 'mid' ? item : null}
 					Mid
-				</div>
-				<div ref={setBottomAreaDroppableRef} className={styles.area}>
-					<DraggableItem id="connection-status">
-						<Item />
-					</DraggableItem>
-				</div>
-				<DragOverlay>{isDragging ? <Item /> : null}</DragOverlay>
+				</Droppable>
+				<Droppable key={'bottom'} id={'bottom'} dragging={isDragging}>
+					{parent === 'bottom' ? item : null}
+					Bottom
+				</Droppable>
+				<DraggableOverlay />
 			</div>
 		</DndContext>
 	);
 };
-
-// import Checkbox from 'components/Checkbox';
-// const LayoutPanel: React.FC = () => {
-// 	return (
-// 		<div className={styles.form}>
-// 			<p className={styles.heading}>Connection status indicator:</p>
-// 			<Checkbox label="Show connection status" />
-// 		</div>
-// 	);
-// };
 
 export default LayoutPanel;
