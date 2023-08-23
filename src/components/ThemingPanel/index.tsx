a9b49112fd19f2c8246703723b22984d3e708a52
Zweihänder <zweidev@zweihander.me>
2023-08-23 16:54:37 -0400

    feat(theming): add in color picker
    Mostly adding in the library at this point. Haven't hooked up the color
    to the front end.

--- src/components/ThemingPanel/index.tsx
+++ src/components/ThemingPanel/index.tsx
@@ -1,5 +1,27 @@
+import * as styles from './style.module.css';
+import { useState } from 'react';
+import { HexColorPicker, HexColorInput } from 'react-colorful';
+
 const ThemingPanel: React.FC = () => {
-	return null;
+	const [untaggedItemBG, setUntaggedItemBG] = useState('#aabbcc');
+	return (
+		<div className={styles.panel}>
+			<label className={styles.label} htmlFor="untaggeditembg">
+				Untagged Item Background:
+			</label>
+			<div className={styles.picker}>
+				<HexColorPicker
+					id="untaggeditembg"
+					color={untaggedItemBG}
+					onChange={setUntaggedItemBG}
+				/>
+			</div>
+			<HexColorInput
+				color={untaggedItemBG}
+				onChange={setUntaggedItemBG}
+			/>
+		</div>
+	);
 };
 
 export default ThemingPanel;
---
+++ src/components/ThemingPanel/style.module.css.d.ts
@@ -0,0 +1,7 @@
+declare const styles: {
+  readonly "panel": string;
+  readonly "label": string;
+  readonly "picker": string;
+};
+export = styles;
+
