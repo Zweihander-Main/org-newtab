46e6d07daac256afbf59c01a2ecce41d63eb8c23
Zweihänder <zweidev@zweihander.me>
2023-09-20 22:53:34 -0400

    test(a11y): add in options a11y tests

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -1,16 +1,51 @@
+import { closeOptions, gotoOptPanel } from './common';
 import { test } from './fixture';
 import { checkA11y, injectAxe } from 'axe-playwright';
 
-test('check accessibility', async ({ page, extensionId }) => {
+const a11yOptionsFront = {
+	detailedReport: true,
+	axeOptions: {
+		rules: {
+			'page-has-heading-one': { enabled: false },
+		},
+	},
+};
+
+test('check accessibility on front page', async ({ page, extensionId }) => {
 	await page.goto(`chrome-extension://${extensionId}/newtab.html`);
 	await injectAxe(page);
 
-	await checkA11y(page, undefined, {
-		axeOptions: {
-			rules: {
-				'page-has-heading-one': { enabled: false },
-			},
-		},
-	});
-	// TODO: go through options menu, sample data, clocked item
+	await checkA11y(page, undefined, a11yOptionsFront);
 });
+
+const a11yOptionsPanels = {
+	detailedReport: true,
+	axeOptions: {
+		rules: {
+			'aria-allowed-attr': { enabled: false },
+			'aria-required-attr': { enabled: false },
+		},
+	},
+};
+
+test('check accessibility on options pages', async ({ page, extensionId }) => {
+	await page.goto(`chrome-extension://${extensionId}/newtab.html`);
+	await injectAxe(page);
+	await gotoOptPanel(page, 'Behavior');
+	await checkA11y(page, undefined, a11yOptionsPanels);
+	await closeOptions(page);
+
+	await gotoOptPanel(page, 'Layout');
+	await checkA11y(page, undefined, a11yOptionsPanels);
+	await closeOptions(page);
+
+	await gotoOptPanel(page, 'Theming');
+	await checkA11y(page, undefined, a11yOptionsPanels);
+	await closeOptions(page);
+
+	await gotoOptPanel(page, 'Debug');
+	await checkA11y(page, undefined, a11yOptionsPanels);
+	await closeOptions(page);
+});
+
+// TODO: test clocked in state
