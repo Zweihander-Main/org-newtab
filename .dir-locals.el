98207b736ef359bd76b9c27d962ed67f8fed580a
Zweihänder <zweidev@zweihander.me>
2023-09-23 20:10:07 -0400

    build(el): explicitly stop ident-tabs

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode . ((indent-tabs-mode . nil)
+					 (ident-tabs-mode . nil)
 					 (fill-column . 80)
 					 (package-lint-main-file . "lisp/org-newtab.el"))))
