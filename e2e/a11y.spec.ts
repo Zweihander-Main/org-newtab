03db60d90871cc0ffbe3d46c21ee85a56892e0aa
Zweihänder <zweidev@zweihander.me>
2024-02-07 15:27:20 -0500

    test(a11y): add clocked in state a11y test

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -1,6 +1,23 @@
-import { closeOptions, gotoOptPanel } from './common';
-import { test } from './fixture';
+import {
+	baseDir,
+	changeTagsFileName,
+	closeOptions,
+	gotoOptPanel,
+	pickARandomPort,
+	setupWebsocketPort,
+	startEmacsProcess,
+	storageIsResolved,
+	testFileName,
+} from './common';
+import {
+	AGENDA_ITEM_TEXT_CLOCKED,
+	AGENDA_ITEM_TEXT_TODO,
+	HOW_LONG_TO_WAIT_FOR_RESPONSE,
+	ITEM_TEXT_LOCATOR,
+} from './constants';
+import { test, expect } from './fixture';
 import { checkA11y, injectAxe } from 'axe-playwright';
+import { promises as fs } from 'fs';
 
 const a11yOptionsFront = {
 	detailedReport: true,
@@ -48,4 +65,36 @@ test('check accessibility on options pages', async ({ page, extensionId }) => {
 	await closeOptions(page);
 });
 
-// TODO: test clocked in state
+test('check accessibility on clocked and non-clocked item', async ({
+	context,
+	extensionId,
+}) => {
+	const port = await pickARandomPort();
+	fs.unlink(testFileName(port)).catch(() => {});
+	fs.unlink(changeTagsFileName(port)).catch(() => {});
+	const emacs = startEmacsProcess(port);
+
+	const tabMaster = await context.newPage();
+	await tabMaster.goto(`chrome-extension://${extensionId}/newtab.html`);
+	await injectAxe(tabMaster);
+	await storageIsResolved(tabMaster);
+	await setupWebsocketPort({ port }, tabMaster);
+	await expect(tabMaster.getByTestId(ITEM_TEXT_LOCATOR)).toContainText(
+		AGENDA_ITEM_TEXT_TODO,
+		{ timeout: HOW_LONG_TO_WAIT_FOR_RESPONSE }
+	);
+
+	await checkA11y(tabMaster, undefined, a11yOptionsFront);
+
+	await fs.copyFile(`${baseDir}/e2e/emacs/clock-in.el`, testFileName(port));
+	await expect(tabMaster.getByTestId(ITEM_TEXT_LOCATOR)).toContainText(
+		AGENDA_ITEM_TEXT_CLOCKED,
+		{ timeout: HOW_LONG_TO_WAIT_FOR_RESPONSE }
+	);
+
+	await checkA11y(tabMaster, undefined, a11yOptionsFront);
+
+	emacs.kill();
+	fs.unlink(testFileName(port)).catch(() => {});
+	fs.unlink(changeTagsFileName(port)).catch(() => {});
+});
